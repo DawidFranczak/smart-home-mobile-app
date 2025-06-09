17ff17ca12784d98e2cc611645cd84086355e6f0
DawidFranczak <daw.franczak@gmail.com>
2025-06-09 19:00:56 +0200

    Fixed background changer functionality

--- smart_home/src/components/BackgroundChanger.tsx
+++ smart_home/src/components/BackgroundChanger.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { Animated, ImageBackground, StyleSheet, View } from "react-native";
 import { usePathname } from "expo-router";
 
 const backgrounds: Record<string, any> = {
@@ -13,6 +13,8 @@ const backgrounds: Record<string, any> = {
   Lamp: require("../../assets/images/lamps_bg.png"),
   Router: require("../../assets/images/router_bg.png"),
 };
+const AnimatedImageBackground =
+  Animated.createAnimatedComponent(ImageBackground);
 const BackgroundChanger: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
@@ -21,15 +23,38 @@ const BackgroundChanger: React.FC<{ children: React.ReactNode }> = ({
   const [currentBg, setCurrentBg] = useState(
     backgrounds[screenName] || backgrounds.Home
   );
+  const [prevBg, setPrevBg] = useState(backgrounds.Home);
+  const oppacityIn = useRef(new Animated.Value(0)).current;
+  const oppacityOut = useRef(new Animated.Value(1)).current;
 
   useEffect(() => {
     const newBg = backgrounds[screenName] || backgrounds.Home;
+    if (currentBg === newBg) return;
     setCurrentBg(newBg);
+    setPrevBg(currentBg);
+    oppacityIn.setValue(0);
+    oppacityOut.setValue(1);
+    Animated.timing(oppacityIn, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(oppacityOut, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
   }, [screenName]);
-
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground source={currentBg} style={styles.background} />
+      <AnimatedImageBackground
+        source={prevBg}
+        style={[styles.background, { opacity: oppacityOut }]}
+      />
+      <AnimatedImageBackground
+        source={currentBg}
+        style={[styles.background, { opacity: oppacityIn }]}
+      />
       <View style={styles.overlay}>{children}</View>
     </SafeAreaView>
   );
