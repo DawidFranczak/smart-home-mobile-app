7422dfa3aedb83565ee1ed293081fd7d1e01c4bf
DawidFranczak <daw.franczak@gmail.com>
2025-06-26 19:53:55 +0200

    Fix cache clearing issue on logout to prevent navigation and state errors

--- smart_home/app/(screen)/Logout.tsx
+++ smart_home/app/(screen)/Logout.tsx
@@ -1,7 +1,7 @@
 import { api } from "@/src/const/api";
 import { useAuth } from "@/src/context/AuthContext";
 import useFetch from "@/src/hooks/useFetch";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
 
@@ -9,11 +9,18 @@ export default function Logout() {
   const { deleteData } = useFetch();
   const { logout } = useAuth();
   const router = useRouter();
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: () => deleteData(api.logout),
     onSuccess: () => {
       logout();
       router.push("/Login");
+      const allQueries = queryClient.getQueryCache().getAll();
+      allQueries.forEach((query) => {
+        if (query.queryKey[0] !== "token") {
+          queryClient.removeQueries({ queryKey: query.queryKey });
+        }
+      });
     },
   });
   useEffect(() => {
